c4718ab6eab51535d985228a3e1ddd31d4e0a5a0
som <pkp927@gmail.com>
2013-07-11 10:11:34 +0530

    first html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
+var fs = require('fs');
+var infile = "index.html"; 
+var buf = new Buffer(30); 
+var text = fs.readFileSync(infile,'utf8');
+buf.write(text,"utf-8");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    response.send(buf.toString('utf-8',0,text.length));
 });
 
 var port = process.env.PORT || 5000;
