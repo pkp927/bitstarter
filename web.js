fc1be79cc8c82feb9efc4126bab8f32415c6d0ba
som <pkp927@gmail.com>
2013-07-11 12:23:24 +0530

    correction1

--- web.js
+++ web.js
@@ -2,9 +2,9 @@ var express = require('express');
 
 var fs = require('fs');
 var infile = "index.html"; 
-var buf = new Buffer(30); 
 var text = fs.readFileSync(infile,'utf8');
-buf.write(text,"utf-8");
+var buf = new Buffer(text,'utf-8');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
